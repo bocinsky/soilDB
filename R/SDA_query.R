12819708abfe9ef1daf0a70a78b9aaa33aedca9b
Bocinsky <bocinsky@bocinskys-mbp.lan>
2019-06-18 12:39:14 -0600

    Avoid writing JSON file at the beginning of

--- R/SDA_query.R
+++ R/SDA_query.R
@@ -10,68 +10,62 @@ format_SQL_in_statement <- function(x) {
 ## chunked queries for large number of records:
 # https://github.com/ncss-tech/soilDB/issues/71
 
-# 
+#
 SDA_query <- function(q) {
   # check for required packages
   if(!requireNamespace('httr', quietly=TRUE) | !requireNamespace('jsonlite', quietly=TRUE))
     stop('please install the `httr` and `jsonlite` packages', call.=FALSE)
-  
-  # temp place to keep json-style post args
-  tf <- tempfile() 
-  
-  # compute json post args and save to temp file
-  # result is JSON with first-line as column names
-  # this means result is a character matrix
-  post.data <- jsonlite::toJSON(list(query=q, format='JSON+COLUMNNAME'), auto_unbox = TRUE)
-  cat(post.data, file=tf, sep = '\n')
-  
+
   # submit request
-  r <- httr::POST(url="https://sdmdataaccess.sc.egov.usda.gov/tabular/post.rest", body=httr::upload_file(tf))
-  
+  r <- httr::POST(url = "https://sdmdataaccess.sc.egov.usda.gov/tabular/post.rest",
+                  body = list(query = q,
+                              format = "JSON+COLUMNNAME"),
+                  encode = "form")
+
   # trap errors, likely related to SQL syntax errors
   request.status <- try(httr::stop_for_status(r), silent = TRUE)
-  
+
   # error message is encapsulated in XML, use xml2 library functions to extract
   if(class(request.status) == 'try-error'){
     # get the request response, this will contain an error message
     r.content <- httr::content(r, as = 'parsed', encoding = 'UTF-8')
     # parse the XML to get the error message
     error.msg <- xml_text(r.content)
-    
+
     ## TODO: error or message?
     stop(error.msg)
   }
-  
-  
+
+
   # the result is JSON:
   # list of character matrix, one for each "Table" returned
   # note: the data returned by SDA/JSON are all character class
   #       we "fix" this later on
   r.content <- httr::content(r, as = 'text', encoding = 'UTF-8')
   d <- jsonlite::fromJSON(r.content)
-  
+
   # number of results
   n.tables <- length(d)
-  
+
   # no results, terminate here
   if(n.tables < 1) {
     message('empty result set')
     return(NULL)
   }
-  
+
   # process list of tables
   # * consistent encoding of NA
-  # * type conversion via read.table() 
+  # * type conversion via read.table()
   # * no conversion strings -> factors: do this on your own
   d <- lapply(d, .post_process_SDA_result_set)
-  
+
   # keep track of SDA result set IDs
   SDA.ids <- names(d)
   for(i in 1:n.tables) {
     attr(d[[i]], 'SDA_id') <- SDA.ids[i]
   }
-  
-  
+
+
   if(n.tables > 1) {
     message('multi-part result set, returning a list')
     return(d)
@@ -80,11 +74,11 @@ SDA_query <- function(q) {
     message('single result set, returning a data.frame')
     return(d[[1]])
   }
-    
-  
-  
-  
-  
+
+
+
+
+
 }
 
 
@@ -94,11 +88,11 @@ SDA_query <- function(q) {
 .post_process_SDA_result_set <- function(i) {
   # the first line is always the colnames
   i.header <- i[1, ]
-  
+
   # remove the first line
   # Arrg! the dreaded sing-row indexing bug: drop=FALSE ensures result is a matrix
   i <- i[-1, , drop=FALSE]
-  
+
   # work-around for all data encoded as char
   # save to file / re-load to guess column classes
   # notes:
@@ -108,15 +102,15 @@ SDA_query <- function(q) {
   #  * use double-quoting in case double quotes are present in the original text
   i.tf <- tempfile()
   write.table(i, file=i.tf, col.names=TRUE, row.names=FALSE, sep='|', quote=TRUE, qmethod = 'double')
-  
+
   # re-load and guess column types
   df <- read.table(i.tf, header=TRUE, sep='|', quote='"', comment.char='', na.strings = c('', 'NA'), stringsAsFactors = FALSE)
- 
+
   # add colnames from original header
   names(df) <- i.header
-  
+
   ## TODO further error checking?
-  
+
   # done
   return(df)
 }
